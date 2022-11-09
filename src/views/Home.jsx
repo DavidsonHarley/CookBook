08ee8a95fadead5ea33a557710701e22017b2a62
Vladislav <vlad.devidson.harle@gmail.com>
2022-11-09 14:31:10 +0300

    bd

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,30 +2,42 @@ const React = require('react');
 const Layout = require('./Layout');
 
 module.exports = function Home({ newUser, receptHome}) {
+  console.log(receptHome);
   return (
     <Layout newUser={newUser}>
       <link rel="stylesheet" href="styles/main.css" />
       Hello user!
       {' '}
       { newUser }
-      <div >
+      
         <div className='mainImg'>
           <img className='img' src="img/1539535829172342793.jpg" alt="edaMain" />
         </div>
-        {receptHome &&  receptHome.map(({title, img, instruction}) => 
+        <div className='containerCards'>
+        {receptHome &&  receptHome.map(({title, img, Compounds}) => 
           <>
           <div className='containerRecept'>
          <div className="card" style={{width: '18rem'}}>
-            <img src={img} class="card-img-top" alt="img"/>
+            <div>
+              <a className='app' href="/">🖤</a> 
+              </div>
+              <a href="/">
+              <img src={img} class="card-img-top" alt="img"/>
+              </a>
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>  
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title">{title}</h5>
+              <div >
+              <p className="card-text">
+                Количество ингредиентов: {Compounds.length}
+                </p> 
+              </div>
             </div>
           </div>
-          </div>
+         </div>
           </>
         )}
-      </div>
+        </div>
+      
     </Layout>
   );
 };
