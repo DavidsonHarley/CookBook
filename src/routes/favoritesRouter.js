26e0723d7d739660ffd27ea756f5eea5b0f5b60b
Alexandr <kalombyslolprox@gmail.com>
2022-11-09 16:12:04 +0300

    console

--- src/routes/favoritesRouter.js
+++ src/routes/favoritesRouter.js
@@ -5,21 +5,11 @@ const { Recept, Favorite, User } = require('../../db/models');
 
 router.get('/', async (req,res) => {
   const { newUser, newUserID } = req.session;
-  // const recept = await Favorite.findAll({raw: true, where : { userID: newUserID }, include: Recept})
   const newRecept = await Favorite.findAll({ include:Recept })
   console.log('132123123', newRecept);
   renderTemplate(Favorites, { newUser, newUserID,newRecept:newRecept.map((el) => el.get({plain:true})) } || null, res);
 })
 
-// router.post('/', async (req, res) => {
-//   const { receptID } = req.body;
-//   const { newUserID } = req.session;
-//   try {
-//     await Favorite.create({userID: newUserID, receptID: receptID });
-//     res.json({ create: 'OK!' });
-//   } catch (error) {
-//     console.log('Ошибка добавления карты в базу!', error);
-//   }
-// });
+
 
 module.exports = router;
